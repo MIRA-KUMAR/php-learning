a4f81fdeea02d5777111213a88c6371cfc0526b1
MIRA-KUMAR <mira.kumar.24@gmail.com>
2023-06-21 15:02:03 -0400

    Learning:

--- index.php
+++ index.php
@@ -2,15 +2,31 @@
 
 <?php 
 
-// First code
+// Hello World
+echo "<h3> First code </h3>";
 
-echo "Hello World! \n";
+echo "Hello World! <br/>";
 
+echo str_repeat("-", 100);
+echo "<br/>";
 
 // Variables 
 
+echo "<h3> Variables </h3>";
 $firstname = "Mira";
 $lastname = "Kumar";
 
-echo "\n My name is ".$firstname ." ".$lastname;
+echo "My name is ".$firstname ." ".$lastname ."<br/>";
+echo str_repeat("-", 100);
+echo "<br/>";
+
+// constants 
+
+echo "<h3> Constants </h3>";
+define('STATUS_PAID', 'paid');
+echo "STATUS_PAID: " .STATUS_PAID;
+
+echo str_repeat("-", 100);
+echo "<br/>";
+
 ?>
